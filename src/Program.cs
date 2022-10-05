77c1ebb69d0a2698b062f8072685bc3411233916
Lucas <LucasClavel356@gmail.com>
2022-10-05 10:25:52 +0200

    comleted first two assigments

---
+++ src/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Net.Sockets;
+
+namespace Pretpark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpListener server = new TcpListener(new System.Net.IPAddress(new byte[] { 127,0,0,1 }), 5000);
+            server.Start();
+            while (true){
+                using Socket connectie = server.AcceptSocket();
+                using Stream request = new NetworkStream(connectie);
+                using StreamReader requestLezer = new StreamReader(request);
+                string[]? regel1 = requestLezer.ReadLine()?.Split(" ");
+                if (regel1 == null) return;
+                (string methode, string url, string httpversie) = (regel1[0], regel1[1], regel1[2]);
+                string? regel = requestLezer.ReadLine();
+                int contentLength = 0;
+                while (!string.IsNullOrEmpty(regel) && !requestLezer.EndOfStream)
+                {
+                    string[] stukjes = regel.Split(":");
+                    (string header, string waarde) = (stukjes[0], stukjes[1]);
+                    if (header.ToLower() == "content-length")
+                        contentLength = int.Parse(waarde);
+                    regel = requestLezer.ReadLine();
+                }
+                if (contentLength > 0) {
+                    char[] bytes = new char[(int)contentLength];
+                    requestLezer.Read(bytes, 0, (int)contentLength);
+                }
+                connectie.Send(System.Text.Encoding.ASCII.GetBytes("HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 11\r\n\r\nHello World"));
+            }
+        }
+    }
+}
